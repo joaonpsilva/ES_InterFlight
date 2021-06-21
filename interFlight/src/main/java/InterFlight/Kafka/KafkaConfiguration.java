d3846742129504c17d467777fa2ae091c5224a90
Miguel SIlva <miguelfsilva1997@gmail.com>
2021-06-21 10:57:14 +0100

    Kafka vm

--- interFlight/src/main/java/InterFlight/Kafka/KafkaConfiguration.java
+++ interFlight/src/main/java/InterFlight/Kafka/KafkaConfiguration.java
@@ -30,12 +30,7 @@ public class KafkaConfiguration {
     public ConsumerFactory<String, String> consumerFactory(){
 
         Map<String, Object> config = new HashMap<>();
-
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.160.18:9092");
-        //config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092"); //Correr LOCAL
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-
+        
         return new DefaultKafkaConsumerFactory(config);
     }
 
